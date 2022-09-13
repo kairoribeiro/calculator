e79740ba92a518af7b0aad9531873c261b685b54
Kairo Ribeiro <kairo10@gmail.com>
2022-09-13 11:57:39 -0400

    add validation

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -45,21 +45,41 @@ class CalcController {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
     }
 
+    pushOperation(value) {
+        this._operation.push(value);
+        if (this._operation.length > 3) {
+            this.calc();
+        }
+    }
+
+    calc() {
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay() {
+        
+    }
+
     addOperation(value) {
         if (isNaN(this.getLastOperation())) {
             if (this.isOperator(value)) {
-                this._setLastOperation(value);
+                this.setLastOperation(value);
             } else if(isNaN(value)) {
-
+                console.log('something else', value);
             } else {
-                this._operation.push(value);
+                this.pushOperation(value);
             }
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+            if (this.isOperator(value)) {
+                this.pushOperation(value);
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+                this.setLastNumberToDisplay()
+            } 
         }
-
-        this._operation.push(value);
     }
 
     setError() {
@@ -89,9 +109,9 @@ class CalcController {
             case 'percentage':
                 this.addOperation('%');
                 break;
-            case 'equal':
-                // this.();
-                break;
+            // case 'equal':
+            //     this.();
+                // break;
                 case 'dot':
                     this.addOperation('.');
                     break;
